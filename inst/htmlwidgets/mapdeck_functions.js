fe9c18f647ca8d67709307f7d0899eff4e715687
Simon Kepplinger <simon.kepplinger@valcome.at>
2019-11-13 09:21:22 +0100

    Simplyfied attempt

--- inst/htmlwidgets/mapdeck_functions.js
+++ inst/htmlwidgets/mapdeck_functions.js
@@ -60,7 +60,9 @@ function md_update_tooltip({x, y, object, layer, index}) {
     // object is the data object sent to the layer function
 
     if ( HTMLWidgets.shinyMode ) {
-        Shiny.onInputChange(layer.props.map_id + "_" + layer + "_hover", layer);
+        console.log(layer);
+        console.log(layer.props.map_id);
+        Shiny.onInputChange(layer.props.map_id + "_" + "test" + "_hover");
     }
 
     if( !md_div_exists( 'mapdecktooltip'+layer.props.map_id ) ) {
